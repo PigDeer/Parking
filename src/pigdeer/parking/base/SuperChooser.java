6a226a60ee85678cf639e2fc2d466cd0ca6f8fa9
zhutao <zhuta0616@gmail.com>
2012-12-02 16:14:23 +0800

    ss

---
+++ src/pigdeer/parking/base/SuperChooser.java
@@ -0,0 +1,27 @@
+package pigdeer.parking.base;
+
+import pigdeer.parking.errors.NoSpaceForCarException;
+import pigdeer.parking.interfaces.ParkLotChooser;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: software
+ * Date: 12-12-2
+ * Time: 下午4:02
+ * To change this template use File | Settings | File Templates.
+ */
+public class SuperChooser implements ParkLotChooser {
+    @Override
+    public ParkLot choose(Parket parket) {
+        double spaceRate = 0.0;
+        ParkLot parkLot = null;
+        for(ParkLot p : parket.getParkingList()){
+            if(p.getSpaceRate()>spaceRate){
+                spaceRate = p.getSpaceRate();
+                parkLot = p;
+            }
+        }
+        if(spaceRate>0) return parkLot;
+        throw new NoSpaceForCarException();
+    }
+}
